3b2e5cfd71bbd8d4d936aa2d0cc44ed507704626
Chu Tuan <tuanct1@vmogroup.com>
2025-12-02 23:56:46 +0700

    Fix bug cannot deploy

--- svelte-fastapi/frontend/svelte-web/svelte.config.js
+++ svelte-fastapi/frontend/svelte-web/svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto'
+import preprocess from 'svelte-preprocess'
+import { plugin as md, Mode } from 'vite-plugin-markdown'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      out: 'dist'
-    })
-  }
-};
+	preprocess: preprocess(),
 
-export default config;
+	kit: {
+		adapter: adapter(),
+		target: '#svelte',
+		vite: {
+			plugins: [
+				md({
+					mode: Mode.HTML,
+					markdownIt: { typography: true },
+				}),
+			],
+		},
+	},
+}
+
+export default config

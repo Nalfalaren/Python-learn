908ae8548cf4db5a08813cead6f759bd44d31d8c
Chu Tuan <tuanct1@vmogroup.com>
2025-12-03 00:06:55 +0700

    Fix bug deployment

--- svelte-fastapi/frontend/svelte-web/svelte.config.js
+++ svelte-fastapi/frontend/svelte-web/svelte.config.js
@@ -1,20 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { plugin as md, Mode } from 'vite-plugin-markdown';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter(), 
-		vite: {
-			plugins: [
-				md({
-					mode: Mode.HTML,
-					markdownIt: { typography: true },
-				}),
-			],
-		},
+		adapter: adapter(),
 	},
 };
 

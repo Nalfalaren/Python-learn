07ab8592c51a7a3fcdee2c735a7c649e6ccc2af3
Chu Tuan <tuanct1@vmogroup.com>
2025-12-02 21:24:47 +0700

    Update

--- svelte-fastapi/fastapi/database.py
+++ svelte-fastapi/fastapi/database.py
@@ -1,9 +1,12 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://postgres:Revive@localhost:5432/db_3"
+# URL_DATABASE = "postgresql://postgres:Revive@localhost:5432/db_3"
+URL_DATABASE = os.getenv("DATABASE_URL")
+
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

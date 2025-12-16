20e6fd651d9db90664a538bb6f03348846125b34
Chu Tuan <tuanct1@vmogroup.com>
2025-12-16 17:15:08 +0700

    Fix test

--- svelte-fastapi/fastapi/database.py
+++ svelte-fastapi/fastapi/database.py
@@ -4,14 +4,14 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 TESTING = os.getenv("TESTING") == "TESTING_ENVIRONMENT"
 
-# if TESTING:
-#   DATABASE_URL = "sqlite:///./test.db"
-# else:
-DATABASE_URL = os.getenv("DATABASE_URL")
+if TESTING:
+  DATABASE_URL = "sqlite:///./test.db"
+else:
+  DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    # connect_args={"check_same_thread": False} if TESTING else {}
+    connect_args={"check_same_thread": False} if TESTING else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
